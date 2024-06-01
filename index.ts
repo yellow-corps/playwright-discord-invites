c9459e0cd3359ccb3d4c61dccc5001ff8afb4b13
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-06-01 11:38:21 +1000

    add a little error handling in case we fail to get a link

--- index.ts
+++ index.ts
@@ -47,57 +47,69 @@ if (mode === Mode.LOGIN) {
   noOfInvites = Number.parseInt(rawNoOfInvites);
 
   const page = await browser.newPage();
-  await page.goto(`https://discord.com/channels/${serverId}/${channelId}`);
-  await page
-    .locator(`a[data-list-item-id="channels___${channelId}"]`)
-    .first()
-    .getByLabel("Create Invite")
-    .click();
+
+  async function reloadPage() {
+    await page.goto(`https://discord.com/channels/${serverId}/${channelId}`);
+    await page
+      .locator(`a[data-list-item-id="channels___${channelId}"]`)
+      .first()
+      .getByLabel("Create Invite")
+      .click();
+  }
+
+  await reloadPage();
 
   for (const i of Array(noOfInvites).keys()) {
-    await expect(await page.getByLabel("Invite link")).not.toHaveValue(
-      "https://discord.gg/"
-    );
+    try {
+      await expect(await page.getByLabel("Invite link")).not.toHaveValue(
+        "https://discord.gg/"
+      );
 
-    const existingInvite = await page.getByLabel("Invite link").inputValue();
+      const existingInvite = await page.getByLabel("Invite link").inputValue();
 
-    await page.getByRole("button", { name: "Edit invite link" }).click();
+      await page.getByRole("button", { name: "Edit invite link" }).click();
 
-    // only need to set the fields on the first open of the edit invite link dialog
-    if (i === 1) {
-      // assuming the correct dropdown by finding the one that says "7 days"
-      await page.getByRole("button", { name: "7 days" }).click();
-      await page.getByRole("option", { name: "Never" }).click();
+      // only need to set the fields on the first open of the edit invite link dialog
+      if (i === 1) {
+        // assuming the correct dropdown by finding the one that says "7 days"
+        await page.getByRole("button", { name: "7 days" }).click();
+        await page.getByRole("option", { name: "Never" }).click();
 
-      // assuming the correct dropdown by finding the one that says "No limit"
-      await page.getByRole("button", { name: "No limit" }).click();
-      await page.getByRole("option", { name: "1 use" }).click();
+        // assuming the correct dropdown by finding the one that says "No limit"
+        await page.getByRole("button", { name: "No limit" }).click();
+        await page.getByRole("option", { name: "1 use" }).click();
+      }
+
+      await page.route(
+        "**/channels/*/invites",
+        async (route) => {
+          const body = route.request().postDataJSON();
+          body.unique = true;
+          await route.continue({ postData: body });
+        },
+        { times: 1 }
+      );
+
+      await page.getByRole("button", { name: "Generate a New Link" }).click();
+
+      await expect(await page.getByLabel("Invite link")).not.toHaveValue(
+        existingInvite
+      );
+
+      await expect(await page.getByLabel("Invite link")).not.toHaveValue(
+        "https://discord.gg/"
+      );
+
+      const inviteLink = await page.getByLabel("Invite link").inputValue();
+      console.log(inviteLink);
+
+      // let's not go too fast, or we might get rate limited lol
+      await page.waitForTimeout(6000);
+    } catch (err) {
+      console.log("failed to get a link, continuing");
+      console.error(err);
+      await reloadPage();
     }
-
-    await page.route(
-      "**/channels/*/invites",
-      async (route) => {
-        const body = route.request().postDataJSON();
-        body.unique = true;
-        await route.continue({ postData: body });
-      },
-      { times: 1 }
-    );
-
-    await page.getByRole("button", { name: "Generate a New Link" }).click();
-
-    await expect(await page.getByLabel("Invite link")).not.toHaveValue(
-      existingInvite
-    );
-
-    await expect(await page.getByLabel("Invite link")).not.toHaveValue(
-      "https://discord.gg/"
-    );
-
-    console.log(await page.getByLabel("Invite link").inputValue());
-
-    // let's not go too fast, or we might get rate limited lol
-    await page.waitForTimeout(6000);
   }
   browser.close();
 }
